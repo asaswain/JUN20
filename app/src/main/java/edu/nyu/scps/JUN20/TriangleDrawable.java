da687af862225786f4e23a49879fe746379fb18e
asaswain <asa_swain@yahoo.com>
2015-06-26 22:29:52 -0400

    Add option to draw using either a brush or a stamp

--- app/src/main/java/edu/nyu/scps/JUN20/TriangleDrawable.java
+++ app/src/main/java/edu/nyu/scps/JUN20/TriangleDrawable.java
@@ -1,28 +1,23 @@
 package edu.nyu.scps.JUN20;
 
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PixelFormat;
 import android.graphics.PointF;
-import android.graphics.drawable.Drawable;
 
 /**
- * Created by swaina on 6/25/15.
+ * This class draws a triangle on the canvas
  */
-public class TriangleDrawable extends Drawable {
+public class TriangleDrawable extends ShapeDrawable {
     private Paint paint;
-    private Paint textPaint = new Paint();
     private PointF center;  //of  triangle
     private float radius;	//of circumscribed circle
 
     public TriangleDrawable(float x, float y, float radius, Paint paint) {
         super();
         this.paint = paint;
-        textPaint.setColor(Color.BLACK);
-        textPaint.setTextSize(radius / 5);
         center = new PointF(x, y);
         this.radius = radius;
         int b = (int) radius;
